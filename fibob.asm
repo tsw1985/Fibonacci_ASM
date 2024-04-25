68003b0ca1c57bfdf8606e31def9c0e1713af054
G85 <gabrielgg85@gmail.com>
2024-04-25 10:04:28 +0100

    G85
    Programa hecho :) . Comentarios finalizados.

--- fibob.asm
+++ fibob.asm
@@ -1,4 +1,4 @@
-; COMPILAR :
+; COMSTACK_SEGMENTR :
 ; 1- nasm fichero.asm -f obj
 ; 2- alink fichero.obj -oEXE
 
@@ -34,17 +34,17 @@ segment DATA_SEGMENT
 	num_a 			      DW   1     				   ; num_a
 	num_b 			      DW   1     				   ; num_b
 	num_c 			      DW   0     				   ; acumulador
-	counter               DW   0      				   ; guardaremos cuantas iteraciones (divisiones) se hicieron
+	division_counter      DW   0      				   ; guardaremos cuantas iteraciones (divisiones) se hicieron
 	counter_iter_fibos    DW   0      				   ; counter para controlar cuantas vueltas llevamos
-	total_iters           DW   12      				   ; total de vueltas fibonaccis que queremos hacers
-	counter_string_number DW   0      				   ; lo usaremos para desplazarnos byte a byte en  
+	total_iters           DW   5      				   ; total de vueltas fibonaccis que queremos hacers
+	pointer_string_number DW   0      				   ; lo usaremos para desplazarnos byte a byte en  
 	next_quotient         DW   0      				   ; NUMERO A VISUALIZAR EN PANTALLA
 ; END DATA	
 
 ; STACK
-segment PILA stack						; segmento de pila
+segment STACK_SEGMENT stack				; segmento de STACK_SEGMENT
 		resb 256			    		; reservamos 256 bytes
-InicioPila:
+START_STACK_SEGMENT:
 	; Aqui están los valores
 	; END STACK
 
@@ -62,8 +62,7 @@ segment CODE_SEGMENT
 										; Una vez impreso el numero 1 ... empieza la fiesta.
 										; en el inicio , hay que darle unos valores a A y B , que son 1 y 1 para que haya un inicio de suma
 
-fibonacci:								; Etiqueta de fibonacci, para cada iteración
-
+FIBONACCI:								; Etiqueta de fibonacci, para cada iteración. Es el inicio del "bucle"
 	XOR AX,AX                   		; ponemos AX a 0
 	MOV AX,DATA_SEGMENT                	; queremos situarnos en el segmento de DATA_SEGMENT ( donde están las variables/espacios de memoria)
 	MOV DS,AX  							; ponemos DS apuntando al segmento de DATA_SEGMENT
@@ -81,8 +80,8 @@ fibonacci:								; Etiqueta de fibonacci, para cada iteración
 										; por lo tanto hay que situar sus punteros para volver a situarnos en su primera posicion
 										; para machacar su antiguo contenido, poniendolos a 0.
 	XOR AX,AX          					; ponemos AX a 0
-	MOV [counter],AX  					; volvemos a poner los counteres de string_number a 0 para resetearlos
-	MOV [counter_string_number],AX 		; volvemos a poner los counteres de string_number a 0 para resetearlos
+	MOV [division_counter],AX  			; ponemos el contador de divisiones 0
+	MOV [pointer_string_number],AX 		; ponemos el contador para string_number a 0
 
 
 	; intercambio de numeros
@@ -108,13 +107,14 @@ fibonacci:								; Etiqueta de fibonacci, para cada iteración
 	MOV BX,[counter_iter_fibos]			; ponemos en BX el nuevo valor de counter_iter_fibos para comparar si ya llegamos al total de iteraciones
 	MOV AX,[total_iters]			    ; ponemos en AX el total de iteraciones
 	CMP AX,BX							; ¿AX es igual a BX ? Es decir, ¿ el contador de iteraciones ya es igual al total de iteracioens ?
-JNE fibonacci							; si NO ES IGUAL , sigue iterando , por lo tanto vuelve al  la etiqueta FIBONACCI
+JNE FIBONACCI							; si NO ES IGUAL , sigue iterando , por lo tanto vuelve al  la etiqueta FIBONACCI
 
 CALL FIN 								;cerramos y finalizamos el programa
 
 
 ; ********************************************************************************
 ; * ESTA RUTINA IMPRIME EL NUMERO QUE ESTÉ GUARDADO EN LA VARIABLE next_quotient *
+; * Y LO ALMACENA EN LA VARIABLE string_number									 *
 ; ********************************************************************************
 PRINT_NUMBER:
 	XOR CX,CX 								; inicia CX a 0
@@ -127,65 +127,60 @@ PRINT_NUMBER:
 		XOR DX,DX           				; ponemos DX a cero porque es donde irá el resto
 		DIV BX              				; ejecutamos la division . La instrucción DIV que guardará en AX el cociente y el resto en DX.
 		MOV [next_quotient], AX				; segun dividimos metemos en next_quotient el cociente
-		PUSH DX             				; guardamos resto en PILA que está en DX
-		MOV CX,[counter]   					; ponemos el valor de counter en CX
-		INC CX              				; incrementamos CX para ir guardando cuantos numeros a mostrar van a la pila
-		MOV [counter],CX     				; guardo en counter el valor de por donde vamos ( cuantos numeros llevamos )
+		PUSH DX             				; guardamos resto en STACK_SEGMENT que está en DX
+		MOV CX,[division_counter]   		; ponemos el valor de counter en CX
+		INC CX              				; incrementamos CX para ir guardando cuantos numeros a mostrar van a la STACK_SEGMENT
+		MOV [division_counter],CX     		; guardo en counter el valor de por donde vamos ( cuantos numeros llevamos )
 		MOV AX,[next_quotient]  	    	; PONEMOS DE NUEVO AX con el valor del cociente actual
 		CMP AX,0 							; ¿ ya el cociente es 0 ?
-		JE PRINT_NUMBER_PROCESS  		; ¿ si ? pues vamos a sacar los numeros
-		JNE CONTINUE_DIV      				; ¿ no ? sigue dividiendo , vuelve a CONTINUE_DIV
+		JE PRINT_NUMBER_PROCESS  			; ¿ si ? pues vamos a sacar los numeros
+	JNE CONTINUE_DIV      					; ¿ no ? sigue dividiendo , vuelve a CONTINUE_DIV
 
-	PRINT_NUMBER_PROCESS:				; A partir de aqui vamos a ir sacando los digitos almacenados en la pila de forma inversa 
+	PRINT_NUMBER_PROCESS:					; A partir de aqui vamos a ir sacando los digitos almacenados en la STACK_SEGMENT de forma inversa 
 											; para ir almacenandolos en string_number, que es la variable que irá a la funcion 9H de INT21h
 		XOR CX,CX				    		; Ponemos CX a 0
-		MOV CX,[counter] 					; ponemos el counter de CX con el total de iteraciones de division
+		MOV CX,[division_counter] 			; ponemos el counter de CX con el total de iteraciones de division
 											; ya que la instruccion LOOP necesita que CX tenga el numero de iteraciones
 
 		POP_MODULE: 						; Empezamos a sacar el resto
-			XOR AX,AX       		    	; ponemos AX a cero
-			MOV AX,DATA_SEGMENT    		    ; guardamos los restos en la variable string_number para luego imprimirla
-			MOV DS,AX       		    	; Nos situamos en el segmento de DATA_SEGMENT
-			POP DX					    	; Sacamos 1 valor de la pila y lo guardamos DX , aquí estan los numeros
-											; apilados de detras a delante 68322 (22386)
-										
-			ADD DX,'0'      		    	; DX contiene el numero guardado pero necesitamos pasarlos a CODE_SEGMENT ASCII
-											; para ello sumamos el 0 (48 en decimal) y obtenemos el digito ASCII que equivale
-											; a ese NUMERO
+			XOR AX,AX                   	; ponemos AX en el segmento de datos		
+			MOV AX,DATA_SEGMENT             ; queremos situarnos en el segmento de DATA_SEGMENT ( donde están las variables/espacios de memoria)
+			MOV DS,AX  						; Nos situamos en el segmento de DATA_SEGMENT
+			POP DX					    	; Sacamos un valor de la pila y lo guardamos en DX , aquí estan los numeros apilados de detras a delante 68322 (22386)
+			ADD DX,'0'      		    	; DX contiene el numero guardado pero necesitamos pasarlos a codigo ASCII para ello sumamos el 0 (48 en decimal)
+											; y obtenemos el digito ASCII que equivale a ese NUMERO
 									
-			MOV BX,[counter_string_number] 	; ponemos en BX el counter para string_number ( estará a 0 ). Es donde almacenaremos los 
-											; numeros para luego pasar su segmento y desplazamiento a la funcion 9h int 21 
-											; (imprimir string_number).
+			MOV BX,[pointer_string_number] 	; ponemos en BX el valor del pointer para string_number ( estará a 0 ). Es donde almacenaremos los 
+											; numeros para luego pasar su segmento y desplazamiento a la funcion 9h int 21 (imprimir string_number).
 
-			MOV [string_number + BX],DX 	  ; Una vez situados en el segmento de DATA_SEGMENT accedemos a la variable string_number 
-											;( espacio de bytes) donde iremos poniendo nuestros numeros para luego verlos.
-											; Iremos avanzando posicion a posicion con BX. [0][1][2][3] ... y poniendo
-											; el resultado de la suma de DX + '0'.
+			MOV [string_number + BX],DX 	; Una vez situados en el string_number accedemos a la variable string_number 
+											;( espacio de bytes) donde iremos poniendo nuestros numeros para luego verlos,usando el puntero pointer_string_number
+											; Iremos avanzando posicion a posicion con BX. [0][1][2][3] ... y poniendo el resultado de la suma de DX + '0'.
+			INC BX                          ; Incrementamos BX para que en la siguiente vuelta sea +1
+			MOV [pointer_string_number],BX  ; guardamos en el counter su numero valor incrementado +1
+			MOV BX,[pointer_string_number]  ; ponemos en BX el puntero de divisiones para saber si ya llegó al final. Si hubieron 4 iteraciones de division, significa
+											; que el numero tiene 4 digitos, por lo tanto tiene que sacar los 4 digitos de la pila, sumar un puntero para añadirlo al lado
+											; del caracter anterior que esta en string_number e ir formando el numero para luego verlo en pantalla
+			MOV AX,[division_counter]		; ponemos en AX el total de divisiones ( digitos que tiene ese numero )
+			CMP AX,BX						; ¿el puntero es igual al total de digitos )
+		JNE POP_MODULE						; si no es igual , pues sigue sacando los restos ( digitos del numero apilados )
 
-
-			INC BX                          ; incrementamos BX para que en la siguiente vuelta sea +1
-			MOV [counter_string_number],BX ; guardamos en el counter su numero valor incrementado +1
-			MOV BX,[counter_string_number]
-			MOV AX,[counter]
-			CMP AX,BX
-		JNE POP_MODULE
-
-		INC BX                      ; le sumo 1 más para añadir el caracter $ que indica fin de string_number 
-		MOV AL,'$'                  ; añadimos el $ al final de la string_number ,uso AL para guardar el caracter $
-		MOV [string_number + BX],AL 	    ; GUARDAMOS EL VALOR $ en el final de la string_number.
-									; imprimimos ya el numero
-									; terminamos la ejecucion del programa
-		MOV AX,DATA_SEGMENT 
-		MOV DS,AX       		    ; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE string_number
-		LEA DX,[string_number]             ; METEMOS EN DX EL OFFSET DE string_number
-		MOV AH,09h                  ; INVOCAMOS AL SERVICIO DE IMPRIMIR string_number EN PANTALLA
-		INT 21h                     ; EJECUTAMOS RUTINA DE IMPRIMIR
-RET						    	; cuando terminemos, pues retornamos
+		INC BX                      		; Como la funcion 9H necesita un $ al final de la cadena a mostrar pues le sumo 1 a BX que contiene el valor del total de digitos
+		MOV AL,'$'                  		; y en esa posicion añadimos el $ al final
+		MOV [string_number + BX],AL 	    ; GUARDAMOS EL VALOR $ en el final de la string_number ( lo que veremos en pantalla )
+											
+											
+		MOV AX,DATA_SEGMENT 				; Nos situamos en el segmento de DATA_SEGMENT
+		MOV DS,AX       		    		; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE string_number
+		LEA DX,[string_number]              ; METEMOS EN DX EL OFFSET DE string_number
+		MOV AH,09h                  		; INVOCAMOS AL SERVICIO DE IMPRIMIR string_number EN PANTALLA
+		INT 21h                     		; EJECUTAMOS RUTINA DE IMPRIMIR
+RET						    				; cuando terminemos, pues retornamos
 	
-FIN:                        	; fin programa
+FIN:                        				; fin programa
 	
-	LEA DX,[bye_bye]              ; METEMOS EN DX EL OFFSET DE string_number
-	MOV AH,09h                  ; INVOCAMOS AL SERVICIO DE IMPRIMIR string_number EN PANTALLA
-	INT 21h                     ; EJECUTAMOS RUTINA DE IMPRIMIR FIN PROGRAMA
-	MOV AH,4Ch                  ; Servicio DOS para finalizar un programa 
-	INT 21h						; lanzamos la int 21h para ejecutarlo.
+	LEA DX,[bye_bye]              			; METEMOS EN DX EL OFFSET DE string_number
+	MOV AH,09h                  			; INVOCAMOS AL SERVICIO DE IMPRIMIR string_number EN PANTALLA
+	INT 21h                     			; EJECUTAMOS RUTINA DE IMPRIMIR FIN PROGRAMA
+	MOV AH,4Ch                  			; Servicio DOS para finalizar un programa 
+	INT 21h									; lanzamos la int 21h para ejecutarlo.
